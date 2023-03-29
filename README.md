69eeb24292bb7039cefa99ca616cc6f868afb74b
akngs <jania902@gmail.com>
2023-03-29 18:48:03 +0900

    패키징 및 이름 변경(nlpip)

--- README.md
+++ README.md
@@ -1,12 +1,59 @@
-# nlpipe A unix pipeline utils based on LLM
+# nlpip A unix pipeline utils based on LLM
 
-`nlpipe` is a small python script intended to be used as a unix pipeline, inspired by
-[Schillace Laws of Semantic AI](https://learn.microsoft.com/en-us/semantic-kernel/howto/schillacelaws).
-
-It's an early prototype written just in a few minutes, yet it's already somewhat useful.
+`nlpip` takes a text written in natural language as input, and performs a
+specified command also written in natural language, and then outputs the result
+in natural language.
 
 Here's a demo:
 
-<p align="center">
-  <img width="600" src="nlpipe.svg" alt="screen recording">
-</p>
+<img width="600" src="nlpip.svg" alt="screen recording">
+
+## Installation
+
+First, you need to install `nlpip` using `pip`:
+
+    pip install nlpip
+
+Then, you need to obtain your OpenAI's API key and set it as an environment
+variable:
+
+    export OPENAI_API_KEY=WRITE_YOUR_KEY_HERE
+
+## How to use
+
+`nlpip` or just `nlp` for short, provides following default commands:
+
+- `keywords`: Extract keywords from input text
+- `poem`: Write poem using input text
+- `summarize`: Summarize input text
+- `sentiment`: Analyze sentiment of input text
+
+For example, if you want summarize `input.txt` and turn it into a poem, you can
+run the following command:
+
+    cat input.txt | nlp summarize | nlp poem
+
+## How to create a new command
+
+To create a new command, you just need to create a text file containing the
+command in natural language. For example, if you want to create a command that
+translates input text into French, you can create a file named
+`~/.nlpip/prompts/fr.txt` in with:
+
+    Translate into French
+
+Then, you can run the following command:
+
+    echo Hello | nlp fr
+
+And you will get the following output:
+
+    Bonjour
+
+You may override default commands by creating a file with the same name in
+`~/.nlpip/prompts/`.
+
+## Credit
+
+- `nlpip` is inspired by
+  [Schillace Laws of Semantic AI](https://learn.microsoft.com/en-us/semantic-kernel/howto/schillacelaws).
---
+++ src/nlpip/__init__.py
@@ -0,0 +1,101 @@
+import hashlib
+import json
+import os
+import re
+import sys
+from pathlib import Path
+
+import openai
+
+HOME = str(Path.home())
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def main():
+    """Run a prompt"""
+    prompt_id = sys.argv[1]
+    template = _load_template(prompt_id)
+    stdin_text = "\n".join(sys.stdin.readlines())
+    prompt = _merge_template(template, stdin_text)
+    answer = _ask(prompt)
+    return re.sub(r"\n\n", "\n", answer)
+
+
+def _load_template(prompt_id):
+    user_path = os.path.join(HOME, ".nlpip", "prompts", f"{prompt_id}.txt")
+    if os.path.exists(user_path):
+        with open(user_path, encoding="utf-8") as f:
+            return f.read()
+
+    builtin_path = os.path.join("prompts", f"{prompt_id}.txt")
+    if os.path.exists(builtin_path):
+        with open(builtin_path, encoding="utf-8") as f:
+            return f.read()
+
+    raise ValueError(f"Prompt {prompt_id} not found")
+
+
+def _merge_template(template, stdin_text):
+    if template.find("{{STDIN}}") != -1:
+        merged = template.replace("{{STDIN}}", stdin_text)
+    else:
+        merged = template + "\n--- START ---\n" + stdin_text + "\n--- END ---\n"
+
+    return merged
+
+
+def _ask(prompt):
+    params = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {
+                "role": "system",
+                "content": "You should answer in clear, concise way.",
+            },
+            {
+                "role": "system",
+                "content": (
+                    "Sentences like '--- START ---' and '--- END ---' are just separators "
+                    "for you. Never include them in your answer."
+                ),
+            },
+            {"role": "user", "content": prompt},
+        ],
+    }
+
+    cache_key = hashlib.sha256(json.dumps(params).encode("utf-8")).hexdigest()
+    cached_answer = _load_cache(cache_key)
+    if cached_answer:
+        return cached_answer
+
+    res = openai.ChatCompletion.create(**params)
+    answer = res["choices"][0]["message"]["content"].strip()
+    _save_cache(cache_key, answer)
+
+    return answer
+
+
+def _load_cache(cache_key):
+    cache_dir = os.path.join(HOME, ".nlpip", "caches", cache_key[:2])
+    os.makedirs(cache_dir, exist_ok=True)
+
+    cache_path = os.path.join(cache_dir, cache_key[2:])
+    if not os.path.exists(cache_path):
+        return None
+
+    with open(cache_path, encoding="utf-8") as f:
+        return f.read()
+
+
+def _save_cache(cache_key, answer):
+    cache_dir = os.path.join(HOME, ".nlpip", "caches", cache_key[:2])
+    os.makedirs(cache_dir, exist_ok=True)
+
+    cache_path = os.path.join(cache_dir, cache_key[2:])
+    with open(cache_path, "w", encoding="utf-8") as f:
+        f.write(answer)
+
+
+if __name__ == "__main__":
+    main()
